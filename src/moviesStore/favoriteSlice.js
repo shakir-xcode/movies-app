e4471a7b3015f120749cde88d7734f67536a19da
Shakir <shaker88999@gmail.com>
2023-07-08 17:54:14 +0530

    fixed favorite error

--- src/moviesStore/favoriteSlice.js
+++ src/moviesStore/favoriteSlice.js
@@ -6,14 +6,14 @@ const slice = createSlice({
         list: [],
         idList: [],
     },
-    // DESC favorite movie structure
+
     reducers: {
         addFavorite: (favorites, action) => {
             favorites.list.push(action.payload)
             favorites.idList.push(action.payload.id)
         },
         removeFavorite: (favorites, action) => {
-            const index = favorites.list.findIndex(movie => movie.id === action.payload.id)
+            const index = favorites.list.findIndex(movie => movie.id === action.payload)
             favorites.list.splice(index, 1)
             favorites.idList.splice(index, 1)
         },
